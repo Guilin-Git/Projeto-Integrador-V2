d664af776268ae6e489fc121aa47e7308cf0ef69
Guilherme Braga <guibragamarinho@gmail.com>
2025-05-11 16:09:16 -0300

    Adicionando Branch DashMedico

--- ProjetoIntegrador/ProjetoIntegrador/obj/Debug/net8.0/ProjetoIntegrador.AssemblyInfo.cs
+++ ProjetoIntegrador/ProjetoIntegrador/obj/Debug/net8.0/ProjetoIntegrador.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoIntegrador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc8482383568392f541f27b3a830ffbbf6c41197")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecfb03039d3d46be7942f6480b4a2b4ee231d48a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoIntegrador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoIntegrador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
