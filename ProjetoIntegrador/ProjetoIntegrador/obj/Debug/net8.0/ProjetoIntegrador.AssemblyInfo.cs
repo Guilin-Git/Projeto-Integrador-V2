77a9c8bfdd1d4cc0920d08decc9e4dec81c682f3
Guilherme Braga <guibragamarinho@gmail.com>
2025-05-11 16:47:17 -0300

    Adicionando Branch Recepcionista

--- ProjetoIntegrador/ProjetoIntegrador/obj/Debug/net8.0/ProjetoIntegrador.AssemblyInfo.cs
+++ ProjetoIntegrador/ProjetoIntegrador/obj/Debug/net8.0/ProjetoIntegrador.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoIntegrador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecfb03039d3d46be7942f6480b4a2b4ee231d48a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d597899551b4a5c94bf8e9ab64e8c29f5772d957")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoIntegrador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoIntegrador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
